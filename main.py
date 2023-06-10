5d8834397e8563220a8537dbb78bfcbcca73eb8c
Nachtschrecken <ferris@uldagon.com>
2023-06-10 16:09:57 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,5 @@
+# Credits to @fermatslibrary on https://twitter.com/fermatslibrary/status/1667517252768333825/photo/1
+#
+# Seeding algorithm to generate new random numbers
+# (c) Ferris Kleier 2023
+# 
